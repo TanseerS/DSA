cbdfb5ab4e7666d3ffccaf3bf2c7651bb1019e72
TanseerS <tanseer@skillminds.in>
2025-08-25 14:57:58 +0530

    typecasting

--- 3-Programming-basic/Conversion.java
+++ 3-Programming-basic/Conversion.java
@@ -1,6 +1,14 @@
-public class Conversion {
+import java.util.Scanner;
 
+public class Conversion {
     public static void main(String[] args) {
-        
+        // java automatically converts datatypes into other datatypes if:
+        // type is compatible --> int to float
+        // float is greater than int
+
+        Scanner input = new Scanner(System.in);
+        float num = input.nextFloat();
+        System.out.println(num);
+        input.close();
     }
 }
