cd59b0951ed2191cbc80d62ac1e9732e9807ca84
TanseerS <tanseer@skillminds.in>
2025-08-26 11:00:33 +0530

    loops

--- 3-Programming-basic/Loops.java
+++ 3-Programming-basic/Loops.java
@@ -1,6 +1,34 @@
+import java.util.Scanner;
+
 public class Loops {
 
     public static void main(String[] args) {
-        
+        // for loop
+        for(int num = 1; num <= 5; num += 1){
+            System.out.println(num);
+        };
+
+        // print number from 1 to n
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        for(int number = 1; number <= n; number += 1){
+            System.out.println(number);
+        };
+        input.close();
+
+        // while loop
+        // while (condition){body}
+        int digit = 1;
+        while(digit<=5){
+            System.out.println(digit);
+            digit += 1;
+        };
+
+        // do-while loop
+        int p = 1;
+        do{
+            System.out.println(p);
+            p += 1;
+        }while(p<=5);
     }
 }
