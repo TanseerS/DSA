a91b5da1f90ce267f5f589511868cdb770e1f0e7
TanseerS <tanseer@skillminds.in>
2025-08-27 10:17:31 +0530

    Intro to Arrays

---
+++ 2-Arrays/Intro.java
@@ -0,0 +1,7 @@
+// Arrays: collection of similar datatypes
+// Syntax: datatype[] variable = new datatype[size] OR datatype[] variable = [element1, element2, ...]
+// datatype[] variable = --> declaration,  new datatype[size] --> initialization
+// declaration happens at compiletime, initialization happens at runtime
+public class Intro {
+    
+}
