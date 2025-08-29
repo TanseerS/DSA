a5f6a7bd1f296c5848dd59a6921d0c0cffcc7c87
TanseerS <tanseer@skillminds.in>
2025-08-29 15:09:30 +0530

    LinearSearch - Strings

--- 2-Arrays/LinearSearch.java
+++ 2-Arrays/LinearSearch.java
@@ -6,6 +6,11 @@ public class LinearSearch {
         int target = 2;
         int ans = linearsearch(arr, target);
         System.out.println(ans);
+
+        String name = "Tanseer";
+        char target1 = 't';
+        boolean ans1 = linearsearch1(name, target1);
+        System.out.println(ans1);
     };
     static int linearsearch(int[] arr, int target){
         if(arr.length == 0){
@@ -18,5 +23,20 @@ public class LinearSearch {
             }
         }
         return -1;
+    };
+
+    // Linear search in String
+
+    static boolean linearsearch1(String str, char target){
+        if(str.length() == 0){
+            return false;
+        };
+        for(int i = 0; i < str.length(); i++){
+            char element = str.charAt(i);
+            if(element == target){
+                return true;
+            };
+        };
+        return false;
     }
 }
