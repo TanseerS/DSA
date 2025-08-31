bb6d1010b33bc22eef32f8a5eadaff3aad249cb8
TanseerS <tanseer@skillminds.in>
2025-08-31 10:03:37 +0530

    Leetcode - 1929

---
+++ 2-Arrays/Leetcode1929.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class Leetcode1929 {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 1};
+        int[] ans = getConcatenation(nums);
+        System.out.println(Arrays.toString(ans));
+    };
+
+    static int[] getConcatenation(int[] nums){
+        int[] ans = new int[2*(nums.length)];
+        for(int i = 0; i < nums.length; i++){
+            ans[i] = nums[i];
+            ans[i+nums.length] = nums[i];
+        }
+        return ans;
+    }
+}
