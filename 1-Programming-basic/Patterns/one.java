ae14dba042afc2657aab88110d28fa7983844fac
TanseerS <tanseer@skillminds.in>
2025-09-04 13:37:05 +0530

    Pattern - Pyramid

--- 1-Programming-basic/Patterns/one.java
+++ 1-Programming-basic/Patterns/one.java
@@ -1,3 +1,7 @@
+// How to approach pattern problems:
+// 1. Outer loop : no. of rows
+// 2. Inner loop : no. of cols (usually wrt rows)
+// 3. What to print
 package Patterns;
 public class one {
     public static void main(String[] args) {
