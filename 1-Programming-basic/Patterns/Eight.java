e011d82b06e7b1b7dabad7ba1d37136fa48b901b
TanseerS <tanseer@skillminds.in>
2025-09-04 14:25:35 +0530

    Pattern - Inverted Pyramid

---
+++ 1-Programming-basic/Patterns/Eight.java
@@ -0,0 +1,19 @@
+package Patterns;
+
+public class Eight {
+    public static void main(String[] args) {
+        int n = 5;
+        pattern(n);
+    };
+    static void pattern(int n){
+        for(int row = 1; row <= n; row++){
+            for(int space = 1; space <= row-1; space++){
+                System.out.print(" ");
+            };
+            for(int star = row; star <= 2*n-row; star++){
+                System.out.print("*");
+            };
+            System.out.println();
+        }
+    }
+}
