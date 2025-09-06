abf6a9b7138f1394edf2ff3d7c27035b384ec4ee
TanseerS <tanseer@skillminds.in>
2025-09-06 10:19:00 +0530

    Pattern - 13

---
+++ Striver'sDsaSheet/Patterns/Pattern13.java
@@ -0,0 +1,16 @@
+public class Pattern13 {
+    public static void main(String[] args) {
+        int n = 5;
+        pattern(n);
+    };
+    static void pattern(int n){
+        int count = 1;
+        for(int row = 1; row <= n; row++){
+            for(int col = 1; col <= row; col++){
+                System.out.print(count+" ");
+                count++;
+            };
+            System.out.println();
+        }
+    }
+}
