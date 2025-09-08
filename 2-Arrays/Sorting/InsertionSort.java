b78158cc7a6da0807a9c60678aafa8e838e245b8
TanseerS <tanseer@skillminds.in>
2025-09-08 09:41:24 +0530

    Insertion Sort

--- 2-Arrays/Sorting/InsertionSort.java
+++ 2-Arrays/Sorting/InsertionSort.java
@@ -1,7 +1,28 @@
+// Time Complexity: o(n^2)
 package Sorting;
+
+import java.util.Arrays;
 
 public class InsertionSort {
     public static void main(String[] args) {
-        
+        int[] nums = {3,5,2,1,4};
+        sort(nums);
+        System.out.println(Arrays.toString(nums));
+    };
+    static void sort(int[] nums){
+        for(int i = 0; i < nums.length -1; i++){
+            for(int j = i+1; j > 0; j--){
+                if(nums[j] < nums[j-1]){
+                    swap(nums , j, j-1);
+                }else{
+                    break;
+                }
+            }
+        }
+    };
+    static void swap(int[] nums, int a, int b){
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
     }
 }
