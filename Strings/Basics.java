3e968243d9c8a84a2b163b6bf38a2cf6d370e0d5
TanseerS <tanseer@skillminds.in>
2025-09-10 09:43:29 +0530

    String Basics

---
+++ Strings/Basics.java
@@ -0,0 +1,13 @@
+// String is class in java
+// Reference of string is stored in stack and object in heap memory
+// There is a string pool(separate memory for string objects) that is used to point same objects of diff refs
+// Strings are thus immutable in java
+// == compares memory location rather than the object itself a.equals(b)
+// .equals() only works on non-primitive datatype 
+// To access a specific char from String use charAt(index)
+package Strings;
+public class Basics {
+    public static void main(String[] args) {
+        
+    }
+}
