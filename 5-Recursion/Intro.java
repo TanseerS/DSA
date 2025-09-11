60fa5e40770df2a06038c55d5322796ad6a58b72
TanseerS <tanseer@skillminds.in>
2025-09-11 14:02:15 +0530

    Intro to Recursion

---
+++ 5-Recursion/Intro.java
@@ -0,0 +1,9 @@
+//when a function its called it stays in the stack till it is executed
+//when a function completes the execution it is removed from the stack and the flow is restored to where the func was called from
+// Recursion: A function calling itself
+public class Intro {
+
+    public static void main(String[] args) {
+        
+    }
+}
